6282989231a3f96c8599aeb1c0b622609cc53512
Rosemwende <rosemuema82@gmail.com>
2023-12-14 05:07:17 -0800

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main  - C program that prints a line to a standard error
- * Return: 1 (Success)
+ * main - A C program that prints a line to a standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
