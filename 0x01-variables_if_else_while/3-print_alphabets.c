a91e29039c55c4028717c32c4a6eba910458f339
Rosemwende <rosemuema82@gmail.com>
2023-12-15 07:02:49 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putcher(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putcher(d);
+			d++;
+		}
+	putcher('\n);
+	return (0);
+}
