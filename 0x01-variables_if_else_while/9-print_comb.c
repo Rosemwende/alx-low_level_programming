e05099d9442502d80ec647018cfa16749c540537
Rosemwende <rosemuema82@gmail.com>
2023-12-15 08:34:14 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,20 @@ int main(void)
 int digit;
 
 for (digit = 0; digit <= 9; digit++)
-}
+{
 putchar(digit + '0');
 
 if (digit < 9)
 {
 putchar(',');
 putchar(' ');
-}
-}
 
+if (digit == 9 - 1)
+{
 putchar('\n');
+}
+}
+}
 
 return (0);
 }
