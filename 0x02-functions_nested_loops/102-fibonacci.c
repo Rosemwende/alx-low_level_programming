1b16187f60d7318903368634adb0d8d4a0fe2153
Rosemwende <rosemuema82@gmail.com>
2023-12-19 14:59:00 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: prints the sum of the even-valued terms,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 long int first = 1, second = 2, next;
