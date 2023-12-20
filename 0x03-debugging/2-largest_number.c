606b508aba2b7421d1813d6da30a04ada70cdffd
Rosemwende <rosemuema82@gmail.com>
2023-12-20 08:48:31 -0800

    2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,22 +7,24 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b && b > c)
 {
 largest = a;
 }
-else if (b > c && b > a)
+else if (b > a && a > c)
 {
 largest = b;
 }
 else
 {
-largest = b;
+largest = c;
 }
 
 return (largest);
 }
+
