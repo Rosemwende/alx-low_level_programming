855a58af1e987b8cefd4900f8664d9c37f60fb0a
Rosemwende <rosemuema82@gmail.com>
2023-12-20 07:40:07 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,17 +10,14 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
-i = 0;
-
-/* Commented out the problematic code to avoid the infinite loop */
-/*
-while (i < 10)
-{
-putchar(i);
-}
+/**
+*i = 0;
+*
+*while (i < 10)
+*{
+*	putchar(i);
+*}
 */
-
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
