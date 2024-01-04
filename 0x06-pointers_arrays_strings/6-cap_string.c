a3eab7a5d0136f434690e2bc69c8e513b4d950a5
Rosemwende <rosemuema82@gmail.com>
2024-01-04 08:06:02 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,33 +3,34 @@
 /**
  * cap_string - capitalizes all words of a string
  * @str: input string
- *
  * Return: pointer to the resulting string
  */
+
 char *cap_string(char *str)
 {
-char *ptr = str;
-int capitalize = 1;
-/* Flag to indicate whether the next character should be capitalized */
+int index = 0;
 
-while (*ptr)
+while (str[index])
 {
-if (capitalize && (*ptr >= 'a' && *ptr <= 'z'))
-{
-*ptr = *ptr - 'a' + 'A';
-capitalize = 0;
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+index++;
 }
-
-if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' ||
-*ptr == ',' || *ptr == ';' || *ptr == '.' || *ptr == '!' ||
-*ptr == '?' || *ptr == '"' || *ptr == '(' || *ptr == ')' ||
-*ptr == '{' || *ptr == '}')
-{
-capitalize = 1;
-}
-
-ptr++;
-}
-
 return (str);
 }
+
