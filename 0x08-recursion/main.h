4a668c6a35bb01c28b168c2b2b2403cb2b814cdf
Rosemwende <rosemuema82@gmail.com>
2024-01-10 03:21:12 -0800

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int calculate_sqrt(int n, int i);
 int check_prime(int n, int i);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
