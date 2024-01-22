bc4aee6815ccb76b6327b607868260dd7fc781b6
Rosemwende <rosemuema82@gmail.com>
2024-01-22 11:22:27 -0800

    Write a function that prints a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - Represents information about a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- */
-struct dog
-{
+struct dog {
 char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,9 +1,7 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef MAIN_H
+#define MAIN_H
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-init_dog(&my_dog, "Poppy", 3.5, "Bob");
 
-#endif /* DOG_H */
+#endif /* MAIN_H */
