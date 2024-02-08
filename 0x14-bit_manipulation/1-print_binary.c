74eaf5d8514a1bb97dd5070a8b92d8dacc16d430
Rosemwende <rosemuema82@gmail.com>
2024-02-08 13:31:50 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,34 +1,49 @@
 #include "main.h"
 
- /**
-  * print_binary - prints out binary equivalent to
-  * a decimal number
-  * @n: The number to print in binary
-  *
-  * Description: This function takes an unsigned long
-  * int and prints its binary representation to
-  * the standard output. It avoids using arrays,
-  * malloc, and the % or / operators.
-  */
+/**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-int i, count = 0;
-unsigned long int current;
+	unsigned long int divisor, check;
+	char flag;
 
-for (i = 60; i >= 0; i--)
-{
-current = n >> i;
-
-if (current & 1)
-{
-_putchar('1');
-count++;
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
 }
-else if (count)
-_putchar('0');
-}
-if (!count)
-_putchar('0');
-}
-
 
