c7a33f0bddfe0dfe2bf8c6f2982b09796fb72f26
Rosemwende <rosemuema82@gmail.com>
2024-03-28 14:37:01 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-get -p .. https://raw.githubusercontent.com/Rosemwende/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+wget -p .. https://raw.githubusercontent.com/Rosemwende/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD="$PWD/../libtest.so"
