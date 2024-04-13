089d16aa147f188e0c8c3a958748c3baeadccc19
Rosemwende <rosemuema82@gmail.com>
2024-04-13 11:36:46 -0700

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a value
+ * @ht: the hash tble to look into
+ * @key: the key
+ *
+ * Return: the value associated with the element
+ * or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index = 0;
+hash_node_t *bucket;
+
+if (!ht || !key || *key)
+return (NULL);
+
+index = key_index((const unsigned char*)key, ht->size);
+bucket = ht->array[index];
+
+while (bucket)
+if (!strcmp(key, bucket->key))
+return (bucket->value);
+bucket = bucket->next;
+}
+return (NULL);
+}
